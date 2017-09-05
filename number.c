bdeabe665ccebaf30728d6902ac0d71402ac7a34
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2017-09-05 16:30:13 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("Enter the numer");
+scanf("%d",&n);
+if(n>0)
+printf("it is positive");
+else if(n<0)
+printf("it is negative");
+esle
+printf("it is equal to zero");
+}
