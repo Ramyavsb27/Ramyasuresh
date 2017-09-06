2d4ae186da872aa69201bbc7f70dd4e1c2caab99
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2017-09-06 17:59:16 +0530

    Rename alp.c to allp.c

---
+++ allp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char x;
+printf("enter the character");
+scanf("%c",&x);
+if(((x>=a)&&(x<=z))//((x>=A))&&(x<=Z)))
+{
+printf("its is alphabet")
+}
+else
+{
+printf("not")
+}
+}
