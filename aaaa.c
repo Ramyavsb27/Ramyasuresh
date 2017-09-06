b2e20a3e1725466d5e21b40c5d2f46bfd1135677
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2017-09-06 17:52:49 +0530

    Create aaaa.c

---
+++ aaaa.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char x;
+printf("enter the character")
+scanf("%c",&x);
+if(x==A//x==E//x==I//x==O//x==U//x=a//x==e//x==i//x==o//x==u)
+{
+printf("it is vowel")
+}
+else
+{
+printf("it is consonant")
+}
+}
