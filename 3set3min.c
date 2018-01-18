553dea91362ab2e889ba2f57b23e04970247084b
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-18 10:30:07 +0000

    Create 3set3min.c

---
+++ 3set3min.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int min,array[i],i,n;
+printf("enter the no.of elements");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+min=array[0];
+scanf("%d",&array[i]);
+if(min>array[i])
+{
+min=array[i];
+i++;
+}
+pintf("min element is %d",min);
+}
+
