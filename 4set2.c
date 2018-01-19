14d4d6e34c6df7fc4ffe9cc44d9093b1e97f36bc
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-19 17:00:41 +0530

    Create 4set2.c

---
+++ 4set2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[100];
+int count=0,i;
+printf("enter the string");
+scanf("%c",&str);
+for(i=0;str[i]!='\0';i++)
+{
+if(s[i]=='')
+count++;
+}
+printf("no.of words %d",count+1);
+}
