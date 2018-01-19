5cd31ee4163679540065c962dbfc21fe47ef2cf5
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-19 15:56:06 +0530

    Create 3setnumeric.c

---
+++ 3setnumeric.c
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int isNumericString(unsigned char *num)
+{
+int i=0;
+while(*(num+i))
+if(*(num+i)>='0'&&(*num+i)<='9')
+i++;
+else 
+return 0;
+}
+return 1;
+}
+int main()
+{
+int r=0;
+unsigned char str[]="123";
+r=isNumericString(str);
+if(r)
+cout<<"it is numeric"<<;
+else
+cout<<"no"<<;
+}
+
