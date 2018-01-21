7d60948b8d1f991bbfd778e09955e486a98930cc
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-21 09:54:28 +0530

    Create 4set35.c

---
+++ 4set35.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int i,count=o;
+int d=0,al=0,sp=0;
+char str[90];
+for(i=0;str[i]!=NULL;i++)
+{
+if(str[i]>='0' && str[i]<='9')
+d++;
+else if(str[i]<='A' && str[i]>='Z')
+al++;
+else if(str[i]=='')
+sp++;
+esle
+count=count++;
+}
+printf("no.of special characters are %d",count);
+}
