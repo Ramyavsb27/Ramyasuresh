89a87afd6476fab4a90dc19bb8dc6a6c70fafce3
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-21 09:23:08 +0530

    Create 4set4.c

---
+++ 4set4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int i,space;
+char x[30];
+printf("enter the string");
+scanf("%s",&x);
+for(i=o;x[i]!="\0";i++)
+{
+if(x[i]='')
+{
+spaces++;
+}
+}
+printf("no.of spaces are %d",spaces);
+}
