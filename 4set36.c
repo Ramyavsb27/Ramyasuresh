e477a162ea5ed78c059a81d3bed103652b21e287
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-21 10:02:50 +0530

    Update 4set36.c

--- 4set36.c
+++ 4set36.c
@@ -4,8 +4,8 @@ void main()
 int a,b,temp;
 printf("enter the value of a and b %d %d",a,b);
 printf("before swapping %d %d",a,b);
-temp=x;
-x=y;
-y=temp;
+temp=a;
+a=b;
+b=temp;
 printf("after swapping %d %d",a,b);
 }
