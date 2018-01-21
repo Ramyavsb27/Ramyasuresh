f7aa2e9ea3375eb42ff19275da541b10fbd7c636
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-21 10:02:22 +0530

    Create 4set36.c

---
+++ 4set36.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,b,temp;
+printf("enter the value of a and b %d %d",a,b);
+printf("before swapping %d %d",a,b);
+temp=x;
+x=y;
+y=temp;
+printf("after swapping %d %d",a,b);
+}
