ced0333cc6fbba55c798f5807c36e6020e51975d
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-21 10:14:38 +0530

    Create 4set39.c

---
+++ 4set39.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int array[10];
+int i;
+int maximum;
+for(i=o;i<10;i++)
+scanf("%d",&array[i);
+maximum=array[0];
+for(i=0;i<10;i++)
+{
+if(array[i]>maximum)
+maximum=array[i];
+}
+printf("maximum among 10 numbers is %d",maximum);
+}
