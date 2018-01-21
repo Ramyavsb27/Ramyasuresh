888a866114070d64dfee9099c46bb06c20ff9d8e
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-21 11:51:15 +0530

    Create 5set44.c

---
+++ 5set44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,low,high;
+printf("enter the number %d",n);
+printf("enter the low value %d",low);
+printf("enter the high value %d",high);
+if((n-low)*(n-high)<=o)
+{
+printf("%d is in the range [%d %d]",n,low,high);
+}
+else
+{
+printf("%d is not in the range [%d %d]",n,low,high);
+}
+}
