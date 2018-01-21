96a68e1bd5ac0894cc0ef305ed2e4f1bc0f493b9
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-21 11:29:14 +0530

    Create 5set42.c

---
+++ 5set42.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void main()
+{
+int count1=0,count2=0;
+char str1[20],str[20],i=0,flag=0;
+printf("enter the string");
+gets(str1);
+printf("enter the string");
+gets(str2);
+while(str[i]!='\0')
+{
+count1++;
+}
+while(str[]!='\0')
+{
+count2++;
+}
+while((i<count1) && (i<count2))
+{
+if(str1[i]==str2[i])
+{
+i++;}
+else if(str1[i]<str[i])
+{
+printf("string1 is less than string string2");
+}
+else if(str[i]>str[i])
+{
+printf("string1 is greater than string2 %s",str[i]);
+}
+}
+}
