0f67666bcb1f5940dd2fff92f8436207d01fcc2d
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-23 16:16:31 +0530

    Create 5set48.c

---
+++ 5set48.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num,i,n,sumo=0;
+float avg;
+printf("enter the number %d",n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&num);
+sum=sum+num;
+}
+avg=sum/n;
+printf("%d",avg);
+}
+
