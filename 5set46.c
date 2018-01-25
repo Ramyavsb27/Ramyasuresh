70dd6dbc86262882106928a25a2d451a6351ff79
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-25 16:17:31 +0530

    Create 5set46.c

---
+++ 5set46.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int numbers[],n,i,max;
+printf("enter the number %d",n);
+for(i=0;i<n;i++)
+scanf("%d",numbers[i]);
+if(numbers[i]==n)
+{
+max=number[i];
+max=max+1;
+}
+for(i=0;i<n-1;i++)
+{
+printf("%d %d",numbers[i],max);
+}
+}
