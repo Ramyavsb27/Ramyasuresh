7536baa31791af7a4f5d4b8eebf4355e3cec619e
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-01-25 16:20:36 +0530

    Update 5set46.c

--- 5set46.c
+++ 5set46.c
@@ -4,10 +4,10 @@ void main()
 int numbers[50],n,i,max;
 printf("enter the number %d",n);
 for(i=0;i<n;i++)
-scanf("%d",numbers[i]);
+scanf("%d",&numbers[i]);
 if(numbers[i]==n)
 {
-max=number[i];
+max=numbers[i];
 max=max+1;
 }
 for(i=0;i<n-1;i++)
