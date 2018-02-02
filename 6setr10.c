8e4f1f35e1ddfd44973bb028bba7897f86d3e539
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-02-02 14:13:42 +0530

    Update 6setr10.c

--- 6setr10.c
+++ 6setr10.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 void main()
 {
-int a=0,b=1,c,n;
+int a=0,b=1,c,n,count=0;
 printf("enter the number");
 scanf("%d",&n);
 printf("%d",a);
 printf("%d",b);
-c=a;
-a=b;
-b=c;
+  copunt=2;
+  while(count<n)
+  {
 c=a+b;
+    count++;
 printf("%d",c);
+    a=b;
+    b=c;
+}
 }
