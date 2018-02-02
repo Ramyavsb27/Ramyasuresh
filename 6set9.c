2989df6a3eadf14016169887619d8877188aac58
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-02-02 14:00:01 +0530

    Update 6set9.c

--- 6set9.c
+++ 6set9.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 void main()
 {
-int a[10];
+int a[30];
 int i,max,n;
-  printf("enter the numbers");
-  scanf("%d",&n);
+printf("enter the numbers");
+scanf("%d",&n);
 for(i=0;i<n;i++)
 scanf("%d",&a[i]);
 max=a[0];
 for(i=0;i<n;i++)
 {
-if(a[i]>max)
+if(max<a[i])
 max=a[i];
 }
 printf("maximum among 10 numbers is %d",max);
