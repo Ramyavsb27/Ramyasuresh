a2055d18c8d32bea63224c8dedc9708cb325cd6c
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-02-02 14:21:32 +0530

    Create 6setmax.c

---
+++ 6setmax.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int s[10],max,i;
+int n;
+printf("enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&s[i]);
+max=s[0];
+if(s[i]>max);
+{
+max=s[i];
+}
+printf("%d",&max);
+}
