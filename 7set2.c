39ed0db409955fbb3d8b2863aa4ac1d25c9a456b
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-02-03 11:19:45 +0000

    Create 7set2.c

---
+++ 7set2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+
+    int i,n;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n%10=='0'&&n%10=='1')
+    {
+    printf("yes binary");
+    }
+    else
+    {
+    printf("no");
+    }
+    
+    
+   
+    
+}
