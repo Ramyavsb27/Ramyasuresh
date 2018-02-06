22baf481a5ca75ea0cf277885d970cf533a46bbd
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-02-06 08:59:45 +0000

    Create 7set8.c

---
+++ 7set8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    printf("enter the number");
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
