5bb2a80633945abe62f93ad7bf2da80c25664476
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-02-09 09:31:26 +0000

    Create 9set2.c

---
+++ 9set2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float a,b,c;
+    printf("enter the length");
+    scanf("%f",&a);
+    printf("enter the breadth");
+    scanf("%f",&b);
+    c=a*b;
+    printf("area of farm is%f",c);
+}
