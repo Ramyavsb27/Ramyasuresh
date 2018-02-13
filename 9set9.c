84f075d2da1db0d5173662a7b6ee674d46e01a56
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-02-13 09:42:58 +0000

    Create 9set9.c

---
+++ 9set9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+char a[100],b[100];
+int i,j,k;
+printf("enter");
+scanf("%d",&a);
+int len=strlen(a);
+for(i=0;i<len;i++)
+{
+    for(j=i+1;j<len;j++)
+    {
+        if(a[i]>a[j])
+        {
+            b[k]=a[i];
+            a[i]=a[j];
+            a[j]=b[k];
+        }
+    }
+    
+}
+for(i=0;i<len;i++)
+printf("%s",a[i]);
+}
