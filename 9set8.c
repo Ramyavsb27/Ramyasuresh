1ee26ff534dda4ef859682d8194ff1cc48c9fd3c
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-02-13 09:27:09 +0000

    Create 9set8.c

---
+++ 9set8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+int a,b,i;
+int lcm,gcd;
+printf("enter the number");
+scanf("%d%d",&a,&b);
+for(i=1;i<=a&&i<=b;i++)
+{
+    if(a%i==0&&b%i==0)
+    gcd=i;
+}
+lcm=(a*b)/gcd;
+printf("%d",lcm);
+}
