aa7905972237685969e6c7fbba7c91827e6c5321
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-02-22 16:32:11 +0530

    Create 10set3.c

---
+++ 10set3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    float tempcel,tempkel;
+    printf("Enter temp in celsius");
+    scanf("%f",&tempcel);
+    tempkel=(tempcel + 273.15);
+    printf("%f",tempkel);
+    
+
+}
