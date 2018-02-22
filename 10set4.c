010adeb4bfa0d713b2f1c5b2b2126fbfd4bc529c
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-02-22 16:40:51 +0530

    Rename 10set3.c to 10set4.c

---
+++ 10set4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int n,k,i,a[2000];
+    printf("Enter twwo number");
+    scanf("%d",&n,&k);
+    printf("numbers are");
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    k=2;
+     for(i=0;i<n;i++)
+     {
+         if(i==k)
+         {
+         printf("%d",a[i]);
+         }
+       
+     }
+  
