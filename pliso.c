2a458278ddaa375994fecb6ccdaee5014e7cd1a6
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-03-08 16:03:30 +0530

    Update pliso.c

--- pliso.c
+++ pliso.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include<string.h>
-int main(void) 
+void main(void) 
 {
-	char str1[50],str2[50];
-	int i,j,flag=1,u,v;
-	scanf("%s %s",str1,str2);
-	int l1=strlen(str1);
-	int l2=strlen(str2);
-	if(l1==l2)
+	char s1[50],s2[50];
+	int i,j,flag=1,a,b;
+	scanf("%s %s",s1,s2);
+	int d1=strlen(s1);
+	int d2=strlen(s2);
+	if(d1==d2)
 	{
-	for(i=0;i<l1;i++)
+	for(i=0;i<d1;i++)
 	{
-		for(j=i+1;j<l2;j++)
+		for(j=i+1;j<d2;j++)
 		{
-			u=str1[i]-str1[j];
-			v=str2[i]-str2[j];
-			if(u==v)
+			a=s1[i]-s1[j];
+			b=s2[i]-s2[j];
+			if(a==b)
 			{
 				flag=0;
 			}
@@ -39,6 +39,4 @@ int main(void)
 	{
 		printf("\nyes");
 	}
-
-	return 0;
 }
