49f83f87a87fc695c165fb98c62aa11aad6fe765
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-03-08 16:44:32 +0530

    Update plswap.c

--- plswap.c
+++ plswap.c
@@ -1,65 +1,31 @@
 #include <stdio.h>
-
 #include<string.h>
-
-int main(void)
-
+void main(void)
 {
-
-	char string[10];
-
-	int i,temp,n;
-
-	gets(string);
-
-	n=strlen(string);
-
-	if(n%2==0)
-
+    char a[10];
+    int i,temp,n;
+    gets(a);
+    n=strlen(a);
+    if(n%2==0)
+    {
+	for(i=0;i<n;i+=2)
 	{
-
-		for(i=0;i<n;i+=2)
-
-		{
-
-			temp=string[i];
-
-			string[i]=string[i+1];
-
-			string[i+1]=temp;
-
+		temp=a[i];
+			a[i]=a[i+1];
+			a[i+1]=temp;
 		}
-
-		puts(string);
-
+		puts(a);
 	}
-
 	else
+	{
+	for(i=0;i<n-1;i+=2)
 
 	{
-
-		for(i=0;i<n-1;i+=2)
-
-		{
-
-			temp=string[i];
-
-			string[i]=string[i+1];
-
-			string[i+1]=temp;
-
+		temp=a[i];
+			a[i]=a[i+1];
+			a[i+1]=temp;
 		}
-
-		puts(string);
-
+		puts(a);
 		
-
-	}
-
-
-
-
-
-return 0;
-
+}
 }
