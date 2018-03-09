e162341c5e10e28672cbba5f9a5112ff1281bb08
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-03-09 16:33:27 +0530

    Create set2pl1.c

---
+++ set2pl1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char s[90];
+    scanf("%s",s);
+    if(s[0]=='s'||s[0]=='S')
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
