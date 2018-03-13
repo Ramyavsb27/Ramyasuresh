f76f35f5cf200c821730056cf004769591125d89
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-03-13 15:50:20 +0530

    Create set2pl17.c

---
+++ set2pl17.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int n,l,h,d,i;
+    scanf("%d%d",&l,&h);
+    if(l>h)
+    {
+        d=l;
+    }
+    else
+    {
+        d=h;
+    }
+    for(i=2;i<=d;i++)
+    {
+        if(i%l==0&&i%h==0)
+        {
+            printf("%d",d);
+        }
+    }
+}
