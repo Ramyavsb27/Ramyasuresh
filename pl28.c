d52e8ba0a66936f4042d8253825ccbff7b3a18dd
Ramyavsb27 <30828830+Ramyavsb27@users.noreply.github.com>
2018-03-16 16:55:01 +0530

    Create pl28.c

---
+++ pl28.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	int i,j=0;
+	char a1[100],b1[200];
+	gets(a);
+	for(i=0;a1[i]!='\0';i++)
+	{
+		if(a1[i]!=' ')
+		{
+            b1[j]=a1[i];
+            j++;
+				}
+			}
+printf("%s",b1);
+}
